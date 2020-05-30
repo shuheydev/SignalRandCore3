fac7acce76af1fda7801ee2d18debb9adcfa094a
Shuhey <shuhey.dev@gmail.com>
2020-05-30 16:50:08 +0900

    Add project files.

---
+++ SignalRandCore3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRandCore3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
